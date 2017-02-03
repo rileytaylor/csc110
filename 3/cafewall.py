54b7991c53e7c987a1c35c0e601f558503074481
Riley <rileyhtaylor@outlook.com>
2017-02-02 20:43:32 -0700

    ITS WORKING!

--- 3/cafewall.py
+++ 3/cafewall.py
@@ -13,7 +13,11 @@ MARGIN = 2
 def main():
     panel = DrawingPanel(650, 400, background="gray")
 
-    draw_grid(panel, 1, 0, 0, 4, 20, 0)
+    draw_grid(panel, 1, 4, 0, 0, 20, 0)
+    draw_grid(panel, 1, 5, 50, 70, 30, 0)
+    draw_grid(panel, 8, 4, 10, 150, 25, 0)
+    draw_grid(panel, 6, 3, 250, 200, 25, 10)
+    draw_grid()
 
 #---------------------------------------------------------------------
 # box() draws a box of a particular color. If it is a black box it will also add
@@ -26,22 +30,24 @@ def main():
 #             color -- a string. The color of the box
 #---------------------------------------------------------------------
 def box(panel, x, y, size, color):
-    panel.canvas.create_rectangle(x, y, x + size, y + size, background=color)
-    if (color == "black"):
-        panel.canvas.draw_line()
-        panel.canvas.draw_line()
+    panel.canvas.create_rectangle(x, y, x + size, y + size, fill=color, outline=color)
+    # if (color == "black"):
+    #     panel.canvas.draw_line()
+    #     panel.canvas.draw_line()
 
 # Draw's a row of a specified length. The count represents a pair.
 def draw_row(panel, count, x, y, size, offset):
-    # TODO: Need to actually add an offset, probs with an if statement
-    # that gives it a bit of a margin before drawing the boxes
-    for pair in range(0, count):
+    for pair in range(1, count):
         box(panel, x, y, size, "black")
-        # Add the size to make up for the black box, might need to add 1?
-        box(panel, x + size, y + size, size, "white")
+        box(panel, x + size + 1, y, size, "white")
+        x = x + size * 2
 
 def draw_grid(panel, rows, row_length, x_start, y_start, box_size, offset):
-    for row in range(0, rows):
+    for row in range(1, rows + 1):
+        # TODO: Add offset
+        if ((row % 2 == 0) and offset > 0):
+            x_start = x_start + offset
         draw_row(panel, row_length, x_start, y_start, box_size, offset)
+        y_start = y_start + box_size + MARGIN
 
 main()
