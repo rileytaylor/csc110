d7b12516e9b8df5adf8e832866156b5eb20a11d4
Taylor, Riley Hal <rileytaylor@catworks.arizona.edu>
2017-02-27 20:47:16 -0700

    refactoring

--- 6/baby_names.py
+++ 6/baby_names.py
@@ -1,5 +1,6 @@
 # Header
 
+from drawingpanel import *
 
 STARTING_YEAR = 1890
 COLUMN_WIDTH = 60
@@ -14,26 +15,34 @@ def main():
     name = input("Name: ")
     gender = input("Gender: ")
     match = find_name("names.txt", name, gender)
+    meaning = find_meaning("meanings.txt", name, gender)
     if (match == ""):
         match = '"' + name + '" not found.'
-    print(match)
+        print(match)
+    else: 
+        print(match + meaning)
+        p = DrawingPanel(780, 560, "white")
+        # draw_basics(p, meaning)
+        # draw_histogram(p, match)
 
 
 def find_name(fname, name, gender):
     f = open(fname)
     names = f.readlines()
-    g = gender[0]
     match = ""
     for n in names:
-        if (n.lower().startswith(name.lower() + " " + g.lower())):
+        if (n.lower().startswith(name.lower() + " " + gender[0].lower())):
             match = n
-    if (match != ""):
-        m = open("meanings.txt")
-        meanings = m.readlines()
-        for m in meanings:
-            if (m.startswith(name.upper())):
-                match += m
     return match
+
+def find_meaning(fname, name, gender):
+    m = open(fname)
+    meanings = m.readlines()
+    for m in meanings:
+        if (m.startswith(name.upper() + " " + gender[0].lower())):
+            return m
+        elif (m.startswith(name.upper() + " mf")):
+            return m
 
 
 # def draw_basics(p, meaning_line):
