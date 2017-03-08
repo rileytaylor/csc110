713da015b00e0bb9bb1211589a9b5e64817dc52d
Taylor, Riley Hal <rileytaylor@catworks.arizona.edu>
2017-03-07 20:18:26 -0700

    finished everything but printing

--- 7/dna.py
+++ 7/dna.py
@@ -12,6 +12,7 @@ MASS_ADENINE = 135.128
 MASS_CYTOSINE = 111.103
 MASS_GUANINE = 151.128
 MASS_THYMINE = 125.107
+MASSES = [MASS_ADENINE, MASS_CYTOSINE, MASS_GUANINE, MASS_THYMINE]
 MASS_JUNK = 100.000
 
 
@@ -28,9 +29,9 @@ def main():
         print("Region Name: " + name)
         print("Nucleotides: " + sequence)
         counts = count_nucleotides(sequence)
-        count_mass(counts)
+        masses = percent_mass(counts, sequence)
         codons = find_codons(sequence)
-        is_protein(codons)
+        is_protein(codons, masses)
         print()
 
 
@@ -41,51 +42,84 @@ def read_data(data):
 
 
 def count_nucleotides(sequence):
-    # Need to use x % 4 to drop count in the right bucket!
-    result = [0, 3, 50, 3]
-    print("Nuc. Counts: " + "[",
-          str(result[0]) + ",",
-          str(result[1]) + ",",
-          str(result[2]) + ",",
-          str(result[3]) + "]")
-    return result
+    counts = [0] * UNIQUE_NUCLEOTIDES
+    for s in range(0, len(sequence)):
+        c = sequence[s]
+        if (c == "A"):
+            counts[0] += 1
+        elif (c == "C"):
+            counts[1] += 1
+        elif (c == "G"):
+            counts[2] += 1
+        elif (c == "T"):
+            counts[3] += 1
+    result = humanize_list(counts)
+    print("Nuc. Counts: " + result)
+    return counts
 
 
-def count_mass(nucelotide_count, sequence):
-    # need to multiply each index by it's constant, then write over it
-    # also need to total up all those, including the total JUNK for a total mass retain
-    result = [100.111, 100.111, 100.111, 100.111]
-    print("Total Mass%: " + "[",
-          str(result[0]) + ",",
-          str(result[1]) + ",",
-          str(result[2]) + ",",
-          str(result[3]) + "]",
-          " of " + str(result[0] + result[1] + result[2] + result[3]))
+def percent_mass(counts, sequence):
+    # Total up the mass of each nucleotide type
+    mass = [0] * UNIQUE_NUCLEOTIDES
+    total_mass = 0
+    for m in range(0, len(mass)):
+        mass[m] = counts[m] * MASSES[m]
+        total_mass += mass[m]
+
+    # Get the amount of junk
+    junk = 0
+    for j in range(0, len(sequence)):
+        dash = sequence[j]
+        if (dash == "-"):
+            junk += 1
+    junk_mass = junk * MASS_JUNK
+    total_mass += junk_mass
+
+    # Let's get the percents!
+    percent_mass = [0] * UNIQUE_NUCLEOTIDES
+    for p in range(0, len(percent_mass)):
+        percent_mass[p] = round((mass[p] / total_mass) * 100, 1)
+    result = humanize_list(percent_mass)
+    print("Total Mass%: " + str(result) + " of " + str(round(total_mass, 1)))
+    return percent_mass
 
 
 def find_codons(sequence):
-    # Set a range step of the NUCLEOTIDES_PER_CODON constant and print the resultant
-    # string, then append it to a list
-    result = ['ATC', 'ATC', 'ATC', 'ATC', 'ATC']
-    print("Codons List: " + "[",
-          str(result[0]) + ",",
-          str(result[1]) + ",",
-          str(result[2]) + ",",
-          str(result[3]) + ",",
-          str(result[4]) + "]")
+    c = []
+    sequence = sequence.replace('-', '')
+    for s in range(0, len(sequence), NUCLEOTIDES_PER_CODON):
+        codon = sequence[s : s + NUCLEOTIDES_PER_CODON]
+        c.append(codon)
+    result = humanize_list(c)
+    print("Codons List: " + result)
+    return c
+
+
+def is_protein(codons, masses):
+    if ((codons[0].startswith("ATG")) and
+        (codons[-1].startswith("TAA") or codons[-1].startswith("TAG") or codons[-1].startswith("TGA")) and
+        (len(codons) >= MIN_CODONS) and
+        (masses[1] + masses[2] >= PERCENTAGE_MASS_CG)):
+        result = "YES"
+    else:
+        result = "NO"
+    print("Is Protein?: " + result)
+
+
+def humanize_list(inhuman_list):
+    # loop through all peices of a list and output a nice, human-readable string!
+    result = "["
+    for i in range(0, len(inhuman_list)):
+        if (i == len(inhuman_list) - 1):
+            result += str(inhuman_list[i])
+        else:
+            result += str(inhuman_list[i]) + ", "
+    result += "]"
     return result
 
 
-def is_protein(codons):
-    # need to loop through the list and count the unique ones (searching lecture?)
-    result = "YES"
-    print("Is Protein?: " + result)
-
-# def humanize_lists(input):
-#     loop through all peices of a list and output a nice HR string!
-
-# def output_results(output, *args):
-#     out = open(output, "w")
+def write_to_file(output, *args):
+    out = open(output, "w")
 
 
 main()
