5fed9b728988589074b8a074f44d9b74524ac65c
Taylor, Riley Hal <rileytaylor@catworks.arizona.edu>
2017-03-20 20:33:36 -0700

    more on 8

--- 8/shelter/animal_shelter.py
+++ 8/shelter/animal_shelter.py
@@ -7,7 +7,7 @@ def main():
     animals = []
     transfers = []
     adoptions = []
-    files = ["pets.txt", "transferred.txt", "adopted.txt"]
+    files = ("pets.txt", "transferred.txt", "adopted.txt")
 
     animals, transfers, adoptions = initiate(files)
 
@@ -26,21 +26,21 @@ def main():
             animals, adoptions = adopt(animals, adoptions, files[0])
         elif (option == "intake"):
             intake_file = input("file? ")
-            animals = intake(intake_file)
+            animals = intake(intake_file, filename)
         elif (option == "list"):
             list_animals(animals)
         elif (option == "quit"):
             done = quit_asm(animals, transfers, adoptions)
         elif (option == "save"):
-            save(animals, transfers, adoptions)
+            save(animals, transfers, adoptions, files)
         elif (option == "transfer"):
             transfer_file = input("file name? ")
             animals, transfers = transfer(animals, transfers, transfer_file)
 
 def initiate(files):
     # Read from all the files and return their contents
-    for f in files:
-        a = read_animals(f)
+    for f in range(0, len(files)):
+        a = read_animals(files[f])
         if (f == 0):
             animals = a
         elif (f == 1):
@@ -60,45 +60,56 @@ def intake(animals, filename):
     for n in new:
         animals.append(n)
     return animals
-    
 
+
+# DONE
 def list_animals(animals):
     animal_type = input("cats, dogs or all? ")
     # List the selected option
+    if (animal_type == "all"):
+        for a in range(0, len(animals)):
+            print(animals[a])
+    elif (animal_type == "cats"):
+        for a in range(0, len(animals)):
+            animal = animals[a]
+            if animal[0].lower() == 'cat':
+                print(animals[a])
+    elif (animal_type == "dogs"):
+        for a in range(0, len(animals)):
+            animal = animals[a]
+            if animal[0].lower() == 'dog':
+                print(animals[a])
+    print()
+                
 
-def quit(animals, transfers, adoptions):
+# DONE
+def quit_asm(animals, transfers, adoptions):
     # print shelter stats
-    print(animals.count() + "pets currently in the shelter")
-    print(adoptions.count() + "adopted")
-    print(transfers.count() + "transferred")
+    print(str(len(animals)) + " pets currently in the shelter")
+    print(str(len(adoptions)) + " adopted")
+    print(str(len(transfers)) + " transferred")
     return True
 
 def save(animals, transfers, adoptions, files):
     # write current shelter list, transferred list, and adopted list 
     # in original format, 
-    for f in files:
-        write_file = open(files[f])
-        write_file.write()
+    for f in range(0, len(files)):
+        write_file = open(files[f], 'w')
         if (f == 0):
-             = animals
+             write_file.write(animals)
         elif (f == 1):
-             = transfers
+             write_file.write(transfers)
         elif (f == 2):
-             = adoptions
+             write_file.write(adoptions)
 
 def transfer(animals, transfers, filename):
-    trans = []
-    f = open(filename)
-    lines = f.readlines()
-    for line in lines: 
-        parts = line.lower().split()
-        animal = (parts[0], parts[1], parts[2])
-        trans.append(animal)
+    trans = read_animals(filename)
     # add to the transfer list, sorted alphabetically
     # remove from the animals list
     return animals, transfers
 
 def read_animals(filename):
+    animals = []
     f = open(filename)
     lines = f.readlines()
     for line in lines: 
