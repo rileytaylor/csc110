18454ff6485664fb16b6dfdfedc827d29c33dd2c
Taylor, Riley Hal <rileytaylor@catworks.arizona.edu>
2017-03-21 18:56:45 -0700

    finished with 8, not really, but whatever

--- 8/shelter/animal_shelter.py
+++ 8/shelter/animal_shelter.py
@@ -1,6 +1,15 @@
+# Author: Riley Taylor
+# Course: CSC 110, Section 2J, Spring 2017
+# Program: Animal Shelter
+#
+# A directory and management tool for animal shelters
+
 # hours Spent: 5
 
-FILES = ("pets.txt", "transferred.txt", "adopted.txt")
+ANIMALS = "pets.txt"
+TRANSFERRED = "transferred.txt"
+ADOPTED = "adopted.txt"
+
 
 def main():
     print("Welcome to animal shelter management software version 1.0!")
@@ -9,7 +18,7 @@ def main():
     transfers = []
     adoptions = []
 
-    animals, transfers, adoptions = initiate(FILES)
+    animals, transfers, adoptions = initiate()
 
     while (done == False):
         print("Type one of the following options:")
@@ -19,11 +28,11 @@ def main():
         print("quit:     exit the program")
         print("save:     save the current data")
         print("transfer: transfer pets to another shelter")
-        
+
         option = input("option? ")
 
         if (option == "adopt"):
-            animals, adoptions = adopt(animals, adoptions, FILES[0])
+            animals, adoptions = adopt(animals, adoptions)
             print()
         elif (option == "intake"):
             intake_file = input("file? ")
@@ -35,7 +44,7 @@ def main():
         elif (option == "quit"):
             done = quit_asm(animals, transfers, adoptions)
         elif (option == "save"):
-            save(animals, transfers, adoptions, FILES)
+            save(animals, transfers, adoptions)
             print()
         elif (option == "transfer"):
             transfer_file = input("file name? ")
@@ -44,22 +53,28 @@ def main():
                                           transfer_file)
             print()
 
-# DONE
+
+# --------------------------------------------------------------------
+# initiate() gets all the data previously saved by the program
+#
+# RETURNS:    the animals, transfers, and adoptions data
+# --------------------------------------------------------------------
 def initiate():
-    # Read from all the FILES and return their contents
-    for f in range(0, len(FILES)):
-        a = read_animals(FILES[f])
-        if (f == 0):
-            animals = a
-        elif (f == 1):
-            transfers = a
-        elif (f == 2):
-            adoptions = a
+    # Read from all the files and return their contents
+    animals = read_animals(ANIMALS)
+    transfers = read_animals(TRANSFERRED)
+    adoptions = read_animals(ADOPTED)
     return animals, transfers, adoptions
 
 
-# DONE
-def adopt(animals, adoptions, filename):
+# --------------------------------------------------------------------
+# adopt() adopts an animal from the shelter
+#
+# PARAMETERS: animals -- a list. animals in the shelter
+#             adoptions -- a list. animals that have been adopted
+# RETURNS:    updated lists of animals and adoptions
+# --------------------------------------------------------------------
+def adopt(animals, adoptions):
     animal_type = input("animal type? ").lower()
     adoptee = input("name? ").lower()
     # If type and name is in the list, remove it and add it to adopted
@@ -73,7 +88,13 @@ def adopt(animals, adoptions, filename):
     return animals, adoptions
 
 
-# DONE
+# --------------------------------------------------------------------
+# intake() pulls in new animals into the shelter
+#
+# PARAMETERS: animals -- a list. animals in the shelter
+#             filename -- a string. where the intake data exists
+# RETURNS:    updated list of animals
+# --------------------------------------------------------------------
 def intake(animals, filename):
     new = read_animals(filename)
     for n in new:
@@ -82,7 +103,12 @@ def intake(animals, filename):
     return animals
 
 
-# DONE
+# --------------------------------------------------------------------
+# list_animals() lists animals currently in the shelter. It will list
+#                all, only cats, or only dogs according to user input
+#
+# PARAMETERS: animals -- a list. animals in the shelter
+# --------------------------------------------------------------------
 def list_animals(animals):
     animal_type = input("cats, dogs or all? ")
     # List the selected option
@@ -99,9 +125,17 @@ def list_animals(animals):
             animal = animals[a]
             if animal[0].lower() == 'dog':
                 print(animals[a])
-                
 
-# DONE
+
+# --------------------------------------------------------------------
+# quit_asm() exits the program and displays statistics about animals
+#            in, transferred, or adopted.
+#
+# PARAMETERS: animals -- a list. animals in the shelter
+#             transfers -- a lits. animals that have been transferred
+#             adoptions -- a list. animals that have been adopted
+# RETURNS:    a boolean to initiate program exit
+# --------------------------------------------------------------------
 def quit_asm(animals, transfers, adoptions):
     # print shelter stats
     print(str(len(animals)) + " pets currently in the shelter")
@@ -110,20 +144,30 @@ def quit_asm(animals, transfers, adoptions):
     return True
 
 
-# DONE
+# --------------------------------------------------------------------
+# save() saves the current data to files
+#
+# PARAMETERS: animals -- a list. animals in the shelter
+#             transfers -- a lits. animals that have been transferred
+#             adoptions -- a list. animals that have been adopted
+# --------------------------------------------------------------------
 def save(animals, transfers, adoptions):
-    # write current shelter list, transferred list, and adopted list 
-    # in original format, 
-    write_file(animals, FILES[0])
-    write_file(transfers, FILES[1])
-    write_file(adoptions, FILES[2])
+    write_file(animals, ANIMALS)
+    write_file(transfers, TRANSFERRED)
+    write_file(adoptions, ADOPTED)
 
 
-# DONE
+# --------------------------------------------------------------------
+# transfer() transfers animals if the animals exist in the shelter
+#
+# PARAMETERS: animals -- a list. animals in the shelter
+#             transfers -- a lits. animals that have been transferred
+#             filename -- a string. file to get which animals will be
+#                         transferred out of the shelter
+# RETURNS: updated animal and transfers data
+# --------------------------------------------------------------------
 def transfer(animals, transfers, filename):
     trans = read_animals(filename)
-    # add to the transfer list, sorted alphabetically
-    # remove from the animals list
     for t in trans:
         for a in animals:
             if a == t:
@@ -134,23 +178,37 @@ def transfer(animals, transfers, filename):
     return animals, transfers
 
 
-# DONE
+# --------------------------------------------------------------------
+# read_animals() reads animal data from a specified file
+#
+# PARAMETERS: filename -- a string. file of animal data to read
+#
+# RETURNS: a list of animals retrieved from a file
+# --------------------------------------------------------------------
 def read_animals(filename):
     animals = []
     f = open(filename)
     lines = f.readlines()
-    for line in lines: 
-        parts = line.lower().split()
-        animal = (parts[0], parts[1], parts[2])
-        animals.append(animal)
+    for line in lines:
+        if line != '':
+            parts = line.lower().split()
+            animal = (parts[0], parts[1], parts[2])
+            animals.append(animal)
     animals.sort()
     return animals
 
-# DONE
+
+# --------------------------------------------------------------------
+# write_file() writes animal data to a specified file
+#
+# PARAMETERS: data -- a list. data to write to the file
+#             filename -- a string. file of animal data to read
+# --------------------------------------------------------------------
 def write_file(data, filename):
     w = open(filename, 'w')
     for d in data:
-        w.write(str(d) + "\n")
+        to_write = str(d[0] + " " + d[1] + " " + d[2])
+        w.write(to_write + "\n")
     w.close()
 
 main()
